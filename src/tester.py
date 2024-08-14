fc3d234418e3572c54e1340b122b28dc3147d184
T0ruv1m <torishima.l@proton.me>
2024-08-14 13:43:02 -0300

    Function reading_database

--- src/tester.py
+++ src/tester.py
@@ -1,11 +1,8 @@
 from config_tools import DIR, EmptyFileMaker
 
 path_to = DIR()
-create_ExtractedData = EmptyFileMaker(path_to.xl_file)
-create_Timestamp = EmptyFileMaker(path_to.timestamp)
-create_json = EmptyFileMaker(path_to.json_file)
+create_ExtractedData = EmptyFileMaker(path_to.extracted_data).empty_file()
+create_Timestamp = EmptyFileMaker(path_to.timestamp).empty_file()
+create_json = EmptyFileMaker(path_to.json_object).empty_file()
 
-create_ExtractedData.empty_file()
-create_Timestamp.empty_file()
-create_json.empty_file()
 
